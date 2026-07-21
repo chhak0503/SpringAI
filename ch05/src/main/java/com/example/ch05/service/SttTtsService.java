ab8edee47b600b4de0df3045edecaa17cbd22ec1
chhak0503 <chhak0503@gmail.com>
2026-07-21 14:57:06 +0900

    add ch05

---
+++ ch05/src/main/java/com/example/ch05/service/SttTtsService.java
@@ -0,0 +1,64 @@
+package com.example.ch05.service;
+
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Service
+public class SttTtsService {
+	
+	private ChatClient chatClient;
+	private OpenAiAudioTranscriptionModel openAiAudioTranscriptionModel;
+	private OpenAiAudioSpeechModel openAiAudioSpeechModel;
+	
+	
+	public SttTtsService(
+			ChatClient.Builder chatClientBuilder, 
+			OpenAiAudioTranscriptionModel openAiAudioTranscriptionModel,
+			OpenAiAudioSpeechModel openAiAudioSpeechModel) {
+		
+		this.chatClient = chatClientBuilder.build();
+		this.openAiAudioTranscriptionModel = openAiAudioTranscriptionModel;
+		this.openAiAudioSpeechModel = openAiAudioSpeechModel;		
+	}	
+	
+	public String stt(String fname, byte[] bytes) {
+		
+		// 파일명 정의
+		Resource resource = new ByteArrayResource(bytes) {
+			@Override
+			public String getFilename() {
+				return fname;
+			}
+		};
+		
+		// 모델 옵션 설정
+		OpenAiAudioTranscriptionOptions options = OpenAiAudioTranscriptionOptions.builder()
+				.model("whisper-1")
+				.language("ko")
+				.build();
+		
+		// 프롬프트 생성
+		AudioTranscriptionPrompt prompt = new AudioTranscriptionPrompt(resource, options);
+		
+		// 모델 요청 및 응답
+		AudioTranscriptionResponse response = openAiAudioTranscriptionModel.call(prompt);
+		String text = response.getResult().getOutput();
+				
+		return text;
+	}
+	
+	public byte[] tts(String text) {	
+		
+		return null;
+	}
+
+}
